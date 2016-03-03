04ad00adf536a86e6018628b231243901e6dfea8
wuzhao <wuzhaozhongguo@hotmail.com>
2016-03-03 19:12:32 +0800

    java环境变量

--- 环境配置/java环境变量配置.bat
+++ 环境配置/java环境变量配置.bat
@@ -1,3 +1,6 @@
 @echo off 
-@SETX JAVA_HOME "C:\Program Files\Java\jdk1.7.0_75" /m
+@set /p javaPath=请输入java安装路径:
+@SETX JAVA_HOME "%javaPath%" /m
+@SETX CLASSPATH "%javaPath%\lib\dt.jar;%javaPath%\lib\tools.jar" /m
+@SETX Path "%javaPath%\bin;%javaPath%\jre\bin;%Path%" /m
 @PAUSE
