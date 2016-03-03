2314d3a032d30b1a15dee3b9f15a98266a764958
wuzhao <wuzhaozhongguo@hotmail.com>
2016-03-03 20:23:11 +0800

    删除

--- 环境配置/java环境变量配置.bat
+++
@@ -1,6 +0,0 @@
-@echo off 
-@set /p javaPath=请输入java安装路径:
-@SETX JAVA_HOME "%javaPath%" /m
-@SETX CLASSPATH "%javaPath%\lib\dt.jar;%javaPath%\lib\tools.jar" /m
-@SETX Path "%javaPath%\bin;%javaPath%\jre\bin;%Path%" /m
-@PAUSE
