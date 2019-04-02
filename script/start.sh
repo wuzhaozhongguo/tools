95d4a107486a004b59e5699e096f33bbff87429d
wuzhaozhongguo <wuzhaozhongguo@hotmail.com>
2019-04-02 10:55:28 +0800

    Update start.sh

--- script/start.sh
+++ script/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 JAR_NAME=`ls *.jar | head -n 1 | awk '{print $0}'`
-nohup java -Djava.compiler=NONE -XX:ParallelGCThreads=2 -Xms64m -Xmx128m -jar ${JAR_NAME} > logs/out.log &
+nohup java -Djava.compiler=NONE -XX:ParallelGCThreads=2 -Xms64m -Xmx128m -jar ${JAR_NAME} > out.log &
 echo $! > tpid
