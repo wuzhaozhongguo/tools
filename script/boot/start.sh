5d7e2ebbe679138e0e92d2ef14016d6df665ef50
wuzhaozhongguo <13991544720@139.com>
2020-06-04 20:23:52 +0800

    restart

--- script/boot/start.sh
+++ script/boot/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 RUN_PARAM_JAVA_OPTS=${1}
 JAR_NAME=`ls *.jar | head -n 1 | awk '{print $0}'`
-nohup java -Djava.security.egd=file:/dev/./urandom ${RUN_PARAM_JAVA_OPTS} -jar ${JAR_NAME} > out.log &
+nohup java -Djava.security.egd=file:/dev/./urandom ${RUN_PARAM_JAVA_OPTS} -jar ${JAR_NAME} > app.log &
 echo $! > tpid
